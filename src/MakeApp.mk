24a10f6b0830318736d70bfd61fad2bf881bbd73
wenfeng.wang@okcoin.com <wenfeng.wang@okcoin.com>
2019-08-21 17:27:03 +0800

    MakeApp add libpcap.a

--- src/MakeApp.mk
+++ src/MakeApp.mk
@@ -11,7 +11,7 @@ OBJS=packet.o connection.o process.o decpcap.o cui.o inode2prog.o conninode.o de
 
 NCURSES_LIBS?=-lncurses
 
-LIBS = /usr/lib/x86_64-linux-gnu/libpcap.a  -lpthread -lm -fPIC
+LIBS = /usr/lib/x86_64-linux-gnu/libpcap.a  -lpthread -lm
 
 .PHONY: check uninstall
 check:
